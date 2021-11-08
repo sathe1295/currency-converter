3347c0741f1627007dea02035790d66643d2b1cc
satheAishwarya <aishwarya.sathe@nitorinfotech.com>
2021-11-09 01:31:56 +0530

    conversion api

--- src/api/currencies.js
+++ src/api/currencies.js
@@ -1,42 +1,45 @@
-
 import {isEmpty} from 'lodash';
 
-export const searchCountryByFullName = async (searchKey) => {
-    let countryObj={}
-  let res = await  fetch(`https://restcountries.com/v2/name/${searchKey}?fullText=true`)
-      if(res.status===200){
-          console.log("rs",res)
-          let json = await res.json();
-          if (!isEmpty(json) && json.length>0) {
-                  console.log("json", json)
-                  json.map(data => {
-                    countryObj['name'] = data.name;
-                    countryObj['population'] = data.population;
-                    countryObj['currencies'] = data.currencies;
-                    countryObj['capital'] = data.capital;
-                    countryObj['flag'] = data.flags.png;
-                  });
-      }
-    } else{
-        alert("Something went wrong")
+export const searchCountryByFullName = async searchKey => {
+  let countryObj = {};
+  let res = await fetch(
+    `https://restcountries.com/v2/name/${searchKey}?fullText=true`,
+  );
+  if (res.status === 200) {
+    let json = await res.json();
+    if (!isEmpty(json) && json.length > 0) {
+      json.map(data => {
+        countryObj['name'] = data.name;
+        countryObj['population'] = data.population;
+        countryObj['currencies'] = data.currencies;
+        countryObj['capital'] = data.capital;
+        countryObj['flag'] = data.flags.png;
+      });
     }
-    console.log("countryobj", countryObj)
-    return countryObj
-}
+  } else {
+    alert('Something went wrong');
+  }
+  if (!isEmpty(countryObj)) {
+    return countryObj;
+  } else {
+    alert('Something went wrong');
+  }
+};
 
-
-export const convertAmount = (amt, code) => {
-    fetch(
-        `https://api.frankfurter.app/latest?amount=${amt}&from=SEK&to=${code}`,
-      )
-        .then(response => response.json())
-        .then(json => {
-            let convertedAmount
-            convertedAmount = json.rates[code];
-          console.log('currency conversion', convertedAmount);
-          return convertedAmount
-        })
-        .catch(err => {
-          console.error(err);
-        });
-}
+export const convertAmount = async (amt, code) => {
+  let convertedAmount = 0;
+  let response = await fetch(
+    `https://api.frankfurter.app/latest?amount=${amt}&from=SEK&to=${code}`,
+  );
+  if (response.status === 200) {
+    let data = await response.json();
+    convertedAmount = data.rates[code];
+  } else {
+    alert('Something went wrong');
+  }
+  if (convertedAmount > 0) {
+    return convertedAmount;
+  } else {
+    alert('Something went wrong');
+  }
+};
--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -1,75 +1,39 @@
 import {isEmpty} from 'lodash';
 import React from 'react';
 
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  Image
-} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, StatusBar, StyleSheet, View, Image} from 'react-native';
 import InputBox from '../components/InputBox';
 import CountryInfo from '../components/CountryInfo';
-import {SEARCH_ICON, CONVERT_ICON, CURRENCY_ICON} from '../assets/index'
-import { searchCountryByFullName } from '../api/currencies';
+import {SEARCH_ICON, CONVERT_ICON, CURRENCY_ICON} from '../assets/index';
+import {convertAmount, searchCountryByFullName} from '../api/currencies';
 
 const MainScreen = () => {
   const [country, setCountry] = React.useState();
   const [showConversion, setShowConversion] = React.useState(false);
-  //const API_KEY = "61b0b5431d64185d5550538d4c0b6f30" fixer.io
-
-  const backgroundStyle = {
-    flex: 1,
-    backgroundColor: "light-gray",
-  };
 
   const onSearch = async searchKey => {
-    let country = await searchCountryByFullName(searchKey)
-    setCountry(country)
+    let country = await searchCountryByFullName(searchKey);
+    setCountry(country);
+    setShowConversion(false);
   };
 
-  const onConvert = amt => {
-    // fetch(`https://data.fixer.io/api/convert
-    // ? access_key = ${API_KEY}
-    // & from = SEK
-    // & to = ${to}
-    // & amount = ${amt}`)
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     console.log('convert', json);
-    //    // setCountry(countriesArray);
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //   });
-        country.currencies.map(currency => {
-          fetch(
-            `https://api.frankfurter.app/latest?amount=${amt}&from=SEK&to=${currency.code}`,
-          )
-            .then(response => response.json())
-            .then(json => {
-
-          console.log('code', currency.code);
-              currency['converted_amount'] = json.rates[currency.code];
-              setShowConversion(true);
-              console.log('currency conversion', currency.converted_amount);
-            })
-            .catch(err => {
-              console.error(err);
-            });
-        });
+  const onConvert = async amt => {
+    country.currencies.map(async currency => {
+      let conversion = await convertAmount(amt, currency.code);
+      currency['converted_amount'] = conversion;
+      setShowConversion(true);
+    });
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar/>
+    <SafeAreaView style={styles.backgroundStyle}>
+      <StatusBar />
       <View style={styles.currencyIconContainer}>
-      <Image source={CURRENCY_ICON} resizeMode={"contain"} style={styles.currencyIcon}/>
+        <Image
+          source={CURRENCY_ICON}
+          resizeMode={'contain'}
+          style={styles.currencyIcon}
+        />
       </View>
       <InputBox
         onButtonPress={onSearch}
@@ -88,8 +52,12 @@ const MainScreen = () => {
   );
 };
 const styles = StyleSheet.create({
-  currencyIconContainer: {alignItems:'center', marginTop:30},
-  currencyIcon: {height:200, width:150}
-})
+  backgroundStyle: {
+    flex: 1,
+    backgroundColor: 'light-gray',
+  },
+  currencyIconContainer: {alignItems: 'center', marginTop: 30},
+  currencyIcon: {height: 200, width: 150},
+});
 
 export default MainScreen;
